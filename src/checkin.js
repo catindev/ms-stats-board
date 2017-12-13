cda2e3fe662ccec1d04035fe63525d3c017d3f56
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-12-13 14:26:05 +0600

    AYBAB2U

--- src/checkin.js
+++ src/checkin.js
@@ -1,6 +1,7 @@
 module.exports = (request, response, next) => {
     const { path, method } = request
     if (path === '/login' && method === 'GET') return next()
+    if (path === '/AYBAB2U' && method === 'GET') return next()  
     
     const { msid } = request.cookies
     if (!msid) return response.sendFile(__dirname + '/static/login/template.html');
--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ app.use(cookieParser())
 app.use(checkIn)
 
 app.get('/', (request, response) => response.redirect('/leads'))
+app.get('/AYBAB2U', (request, response) => {
+    const { query: { msid } } = request
+    response.cookie('msid', msid, { maxAge: 900000, httpOnly: true })
+    response.redirect('/leads')
+})
 app.get('*', (request, response) => response.sendFile(__dirname + '/static/app/template.html'))
 
 const listener = app.listen(5010, () => console.log('Started at', listener.address().port))
