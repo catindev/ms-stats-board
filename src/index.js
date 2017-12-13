ba395fccd534d4918147b3c342a93fcba704f8e5
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-12-13 14:35:32 +0600

    AYBAB2U

--- src/index.js
+++ src/index.js
@@ -12,8 +12,13 @@ app.use(checkIn)
 app.get('/', (request, response) => response.redirect('/leads'))
 app.get('/AYBAB2U', (request, response) => {
     const { query: { msid } } = request
-    response.cookie('msid', msid, { maxAge: 900000, httpOnly: true })
+    response.cookie('msid', msid, { maxAge: 3600000, path: '/' });
     response.redirect('/leads')
+})
+app.get('/exit', (request, response) => {
+    const { query: { msid } } = request
+    response.clearCookie('msid')
+    response.redirect('/') 
 })
 app.get('*', (request, response) => response.sendFile(__dirname + '/static/app/template.html'))
 
