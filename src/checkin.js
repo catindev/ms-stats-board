265e2c90c78fde75313c5ef5ea033df3bb0f5b65
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2018-01-08 17:13:24 +0600

    Partner stats workaround

--- src/checkin.js
+++ src/checkin.js
@@ -3,10 +3,12 @@ module.exports = (request, response, next) => {
     if (path === '/fuckoff' && method === 'GET') return next()
     if (path === '/iddqd' && method === 'GET') return next()
     if (path === '/login' && method === 'GET') return next()
-    if (path === '/AYBAB2U' && method === 'GET') return next()  
-    
+    if (path === '/AYBAB2U' && method === 'GET') return next()
+
+    if (path === '/partner_1515409671435' && method === 'GET') return next()
+
     const { msid } = request.cookies
     if (!msid) return response.sendFile(__dirname + '/static/login/template.html');
-    
+
     next()
 }
